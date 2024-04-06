761862a2b1aefead18f8dccfec2f108bda8675f9
Milad Joodi <winbeta.com@gmail.com>
2024-04-06 14:45:06 +0330

    up

--- app/_components/user3/UserList3.tsx
+++ app/_components/user3/UserList3.tsx
@@ -3,13 +3,15 @@ import SingleUser3 from './SingleUser3'
 import { getUser } from '@/app/_components/user3/Action3'
 
 async function UserList3() {
-
-    const users = await getUser()
+const UserList3 = ()=>{
+  
+}
+    const users = await getUser();
     // console.log(users)
 
 
   return (
-    <div className="w-full">
+    <div className="w-full bg-slate-200 p-5">
        {users !== undefined && users.map((user: IUSER, index: number)=>(
                 <SingleUser3 key={index} user={user} />
        ))}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import Link from "next/link";
 
-const poppins  = Poppins({
+const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
   display: "swap",
@@ -21,17 +22,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins .className} bg-gray-200 px-5`}>
-        <main className="bg-white max-w-5xl px-5 mx-auto min-h-[100dvh]">
-        {children}
+      <body className={`${poppins.className} bg-gray-200 px-5`}>
+        <main className="bg-white max-w-5xl px-5 mx-auto min-h-[100dvh] py-10 gap-10 flex flex-col">
+          <ul className="w-full bg-slate-950 text-white flex flex-row gap-5 uppercase">
+            <li>
+              <Link href={"/"} className="py-2 px-5 flex hover:text-slate-300">
+              Home</Link>
+            </li>
+            <li>
+              <Link href={"/user"} className="py-2 px-5 flex hover:text-slate-300">
+              User</Link>
+            </li>
+          </ul>
+
+          {children}
         </main>
         <Toaster
-        position="top-right"
-        toastOptions={{
-          duration: 10000,
-        }}
+          position="top-right"
+          toastOptions={{
+            duration: 10000,
+          }}
         />
-        </body>
+      </body>
     </html>
   );
 }
